71b9474d9b693c2c35c0e18ed5f5a9d15ebe861c
Your Name <you@example.com>
2021-11-28 07:23:24 +0700

    ucfirst

--- resources/views/emails/orders/shipped.blade.php
+++ resources/views/emails/orders/shipped.blade.php
@@ -1,7 +1,7 @@
 @component('mail::message')
 # Welcome
 
-Hi, {{ $user->name }}
+Hi, {{ ucfirst($user->name) }}
 Welcome to Tutorial Mailtrap, your account has been created successfully.
 
 @component('mail::button', ['url' => 'http://127.0.0.1:8000'])
